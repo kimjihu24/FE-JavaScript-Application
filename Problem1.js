d359b102c9ea4aa93b052658e5f0d814b095547e
kimjihu24 <wlgn0204@naver.com>
2024-04-06 17:43:34 +0900

    1번 문제

--- Problem1.js
+++ Problem1.js
@@ -2,12 +2,12 @@
 
 let Name = "주용";
 
-// ? 부분에 예측 값을 작성해주세요(코드 실행 금지!!!)
+// 주용
 
-alert(`hello ${1}`); // ?
+alert(`hello ${1}`); // hello 1
 
-alert(`hello ${"Name"}`); // ?
+alert(`hello ${"Name"}`); // hello Name
 
-alert("hello ${Name}"); // ?
+alert("hello ${Name}"); // hello ${Name}
 
-alert(`hello ${Name}`); // ?
+alert(`hello ${Name}`); // hello 주용
